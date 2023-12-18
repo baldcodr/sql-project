cf183b510d10b9285dee9eb78167e428505b73a6
David Akuma <chukay360@gmail.com>
2023-12-18 18:35:56 +0000

    added new sales sql file
    from the niyo sql project

---
+++ sales.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM sales.customers;
+
+SELECT *
+FROM sales.markets;
+
+SELECT *
+FROM sales.products;
+
+SELECT *
+FROM sales.date;
+
+SELECT *
+FROM sales.transactions;
+
+-- 1. Which market has the highest number of customers;
+-- 2. Which product had the most sales in 2019
+-- 3. Which cumsotmer bought the most products between 2017 and 2019 
+-- 4. Which market names have the most sales
+-- 5. How many product are in each of the markets names
